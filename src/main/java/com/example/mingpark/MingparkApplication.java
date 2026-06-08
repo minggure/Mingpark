292591f17cab0d1a6c0e6bb38afee11c9a48278b
YunTaeng <howard9166@naver.com>
2026-06-08 17:19:10 +0900

    폴더 세분화

--- src/main/java/com/example/mingpark/MingparkApplication.java
+++ src/main/java/com/example/mingpark/MingparkApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication // 🎯 괄호 안에 있던 exclude 어쩌구를 싹 지워버려!
 public class MingparkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MingparkApplication.class, args);
     }
-
 }
