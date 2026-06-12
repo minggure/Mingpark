749eb193e0eb2ad7282186c636763423fbf0df49
민규 <zxzx013@naver.com>
2026-06-12 15:00:04 +0900

    공연 등록하기 추가

--- src/main/java/com/example/mingpark/service/ConcertService.java
+++ src/main/java/com/example/mingpark/service/ConcertService.java
@@ -2,9 +2,15 @@ package com.example.mingpark.service;
 
 
 import com.example.mingpark.domain.Concert;
+import com.example.mingpark.domain.ConcertStatus;
+import com.example.mingpark.dto.ConcertCreatRequesstDto;
 import com.example.mingpark.dto.ConcertResponseDto;
 import com.example.mingpark.repository.ConcertRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,5 +28,24 @@ public class ConcertService {
         return concerts.stream()
                 .map(ConcertResponseDto::new)
                 .collect(Collectors.toList());
+    }
+    // 공연 추가 하기
+    public void createConcert(ConcertCreatRequesstDto request){
+        Concert concert = Concert.builder()
+                .concertTitle(request.getConcertTitle())
+                .concertDate(request.getConcertDate())
+                .concertTime(request.getConcertTime())
+                .concertPrice(request.getConcertPrice())
+                .image(request.getImage())
+                .status(ConcertStatus.valueOf("UPCOMING")) //기본값
+                .build();
+
+        concertRepository.save(concert);
+    }
+    public Page<ConcertResponseDto> getConcerts(int page, int size){
+        Pageable pageable = PageRequest.of(page, size, Sort.by("concertId").descending());
+        Page<Concert> concertPage = concertRepository.findAll(pageable);
+
+        return concertPage.map(ConcertResponseDto::new);
     }
 }
