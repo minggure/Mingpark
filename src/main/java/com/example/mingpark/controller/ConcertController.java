1ed9bb44ce24e8e6373e427434c039d569dde60f
YunTaeng <howard9166@naver.com>
2026-06-12 15:15:50 +0900

    merge 진행
    merge

--- src/main/java/com/example/mingpark/controller/ConcertController.java
+++ src/main/java/com/example/mingpark/controller/ConcertController.java
@@ -1,11 +1,11 @@
 package com.example.mingpark.controller;
 
+import com.example.mingpark.dto.ConcertCreatRequesstDto;
 import com.example.mingpark.dto.ConcertResponseDto;
 import com.example.mingpark.service.ConcertService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +16,20 @@ public class ConcertController {
 
     private final ConcertService concertService;
 
-    @GetMapping("/concerts")
-    public List<ConcertResponseDto> getConcerts() {
-        return concertService.getAllConcerts();
+//    @GetMapping("/api/concerts")
+//    public List<ConcertResponseDto> getConcerts() {return concertService.getAllConcerts();
+//    }
+    @PostMapping("/api/concerts")
+    public String createConcert(@RequestBody ConcertCreatRequesstDto request){
+        concertService.createConcert(request);
+        return "공연 등록 완료";
+    }
+
+    @GetMapping("/api/concerts")
+    public Page<ConcertResponseDto> getConcerts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "6") int size
+    ) {
+        return concertService.getConcerts(page, size);
     }
 }
