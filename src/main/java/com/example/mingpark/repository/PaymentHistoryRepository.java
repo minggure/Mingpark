363c3337d2b6821d457464038a63c13a18cda9de
민규 <zxzx013@naver.com>
2026-07-07 11:24:55 +0900

    fix : 공연 수정 삭제 오류해결

--- src/main/java/com/example/mingpark/repository/PaymentHistoryRepository.java
+++ src/main/java/com/example/mingpark/repository/PaymentHistoryRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface PaymentHistoryRepository extends JpaRepository<PaymentHistory, Long> {
 
-    // 예매 내역(reservation)을 통해 해당 공연(concertId)의 결제 내역을 찾아 한 방에 삭제!
+
     @Modifying
     @Query("DELETE FROM PaymentHistory p WHERE p.reservation.concert.concertId = :concertId")
     void deleteAllByConcertId(@Param("concertId") Long concertId);
